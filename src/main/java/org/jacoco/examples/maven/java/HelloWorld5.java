9f8b1a057e7bf355879ee5313e206804b698dc40
husofskyzy <15997168992@163.com>
2022-04-11 19:59:00 +0800

    提交

---
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld5.java
@@ -0,0 +1,10 @@
+package org.jacoco.examples.maven.java;
+
+public class HelloWorld5 {
+    public void show333(){
+        System.out.println("show time");
+    }
+    public void show666(){
+        System.out.println("show time");
+    }
+}
