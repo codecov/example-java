cf7246ca530a4d92439dccdfcbc4e4d158523fd3
Tom Hu <tom@codecov.io>
2022-08-15 12:48:18 -0700

    Update calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,23 @@
 package calculator;
 
 public class Calculator {
-  public static int add(int x, int y) {
+  public static double add(double x, double y) {
     return x + y;
+  }
+
+  public static double subtract(double x, double y) {
+    return x - y;
+  }
+
+  public static double multiply(double x, double y) {
+    return x * y;
+  }
+
+  public static double divide(double x, double y) {
+    if (y == 0) {
+      System.out.println("Cannot divide by 0");
+      return 0;
+    }
+    return x / y;
   }
 }
