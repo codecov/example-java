7d5ff74696f1b92b4ac950284809fc8232497cce
Tom Hu <tom@codecov.io>
2022-08-15 12:52:46 -0700

    Update tests

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculatorTest {
+  private static final double DELTA = 0.001;
+
   @Test
   public void testAdd() {
-    assertEquals(new Calculator().add(1, 1), 2);
+    assertEquals(new Calculator().add(1, 1), 2, DELTA);
   }
 }
